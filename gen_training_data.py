923216e943def66a9d5530ca161199d1cc55f6b5
Sam Hatfield <samuel.hatfield@physics.ox.ac.uk>
2018-12-12 11:12:08 +0000

    Updated training data generation

--- gen_training_data.py
+++ gen_training_data.py
@@ -1,13 +1,16 @@
-from lorenz96 import step
-from model_params import nx, ny, δt
+from lorenz96 import ThreeLayerLorenz96Model
+from model_params import nx, ny, nz, δt
 import numpy as np
 
 # Length of run
-N = 2010000
+N = int(2010000.0 * 1.0/δt)
 
 # Initialise state vector array
-state_1 = state_2 = np.zeros(nx+nx*ny)
+state_1 = state_2 = np.zeros(nx+nx*ny+nx*ny*nz)
 state_1[0] = 8.0
+
+# Initialise numerical model
+num_model = ThreeLayerLorenz96Model()
 
 # Generate training data
 with open('training_data.txt', 'w') as training_data_file:
@@ -16,8 +19,9 @@ with open('training_data.txt', 'w') as training_data_file:
             print(f"Time step {i}")
             
         # Step forward once
-        state_2 = step(state_1)
+        state_2 = num_model.step(state_1)
         
+        # Write training data every 1.0 model time unit
         if i%(1.0/δt) == 0:
             # Form training data and write to file
             training_data_line = np.concatenate((state_1[:nx], state_2[:nx] - state_1[:nx]))
--- lorenz96.py
+++ lorenz96.py
@@ -1,38 +1,61 @@
 import numpy as np
-from model_params import F, h, c, b
+from model_params import *
 
-"""
-Step model state forward by one timestep
-"""
-def step(previous):
-    from model_params import δt
+class TwoLayerLorenz96Model:
+    """
+    Step model state forward by one timestep
+    """
+    def step(self, previous):    
+        # 4th order Runge-Kutta
+        k1 = self.ode(previous)
+        k2 = self.ode(previous+0.5*δt*k1)
+        k3 = self.ode(previous+0.5*δt*k2)
+        k4 = self.ode(previous+δt*k3)
+
+        return previous + (δt/6)*(k1 + 2*k2 + 2*k3 + k4)
+
+    """
+    ODE for two layer Lorenz '96 system
+    """
+    def ode(self, state):    
+        # Break up state vector into X, Y and Z components
+        x = state[:nx]
+        y = state[nx:nx+nx*ny]
+
+        return np.concatenate((self.dxdt(x,y), self.dydt(x,y)))
+
+    """
+    Individual ODEs for X and Y variables
+    """
+    def dxdt(self, x, y):    
+        sum_y = np.sum(np.split(y, x.shape[0]), axis=1)
+        return (np.roll(x, -1) - np.roll(x, 2))*np.roll(x, 1) - x + F - (h*c/b)*sum_y
+
+    def dydt(self, x, y):
+        x_rpt = np.repeat(x, y.shape[0]/x.shape[0])
+        return (np.roll(y, 1) - np.roll(y, -2))*c*b*np.roll(y, -1) - c*y + (h*c/b)*x_rpt\
+
+class ThreeLayerLorenz96Model(TwoLayerLorenz96Model):
+    """
+    ODE for three layer Lorenz '96 system
+    """
+    def ode(self, state):    
+        # Break up state vector into X, Y and Z components
+        x = state[:nx]
+        y = state[nx:nx+nx*ny]
+        z = state[nx+nx*ny:]
+
+        return np.concatenate((self.dxdt(x,y), self.dydt(x,y,z), self.dzdt(y,z)))
+
+    """
+    Individual ODEs for X, Y and Z variables
+    """
+    def dydt(self, x, y, z):
+        x_rpt = np.repeat(x, y.shape[0]/x.shape[0])
+        sum_z = np.sum(np.split(z, y.shape[0]), axis=1)
+        return (np.roll(y, 1) - np.roll(y, -2))*c*b*np.roll(y, -1) - c*y + (h*c/b)*x_rpt\
+            - (h*e/d)*sum_z
     
-    # 4th order Runge-Kutta
-    k1 = ode(previous)
-    k2 = ode(previous+0.5*δt*k1)
-    k3 = ode(previous+0.5*δt*k2)
-    k4 = ode(previous+δt*k3)
-
-    return previous + (δt/6)*(k1 + 2*k2 + 2*k3 + k4)
-
-"""
-ODE for whole Lorenz '96 system
-"""
-def ode(state):
-    from model_params import nx, ny
-    
-    x = state[:nx]
-    y = state[nx:]
-
-    return np.concatenate((dxdt(x,y), dydt(x,y)))
-
-"""
-Individual ODEs for X and Y variables
-"""
-def dxdt(x, y):    
-    sum_y = np.sum(np.split(y, x.shape[0]), axis=1)
-    return (np.roll(x, -1) - np.roll(x, 2))*np.roll(x, 1) - x + F - (h*c/b)*sum_y
-
-def dydt(x, y):
-    x_rpt = np.repeat(x, y.shape[0]/x.shape[0])
-    return (np.roll(y, 1) - np.roll(y, -2))*c*b*np.roll(y, -1) - c*y + (h*c/b)*x_rpt
+    def dzdt(self, y, z):
+        y_rpt = np.repeat(y, z.shape[0]/y.shape[0])
+        return (np.roll(z, -1) - np.roll(z, 2))*e*d*np.roll(z, 1) - g_z*e*z + (h*e/d)*y_rpt
