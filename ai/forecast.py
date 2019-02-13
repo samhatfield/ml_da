85639b420a35327534c6fdaa691501106dc25b36
Sam Hatfield <samuel.hatfield@physics.ox.ac.uk>
2019-02-13 14:56:28 +0000

    Neural net forecasting script now gets initial state from training data

--- ai/forecast.py
+++ ai/forecast.py
@@ -4,6 +4,7 @@ from numerical_model.qg_constants import qg_constants as const
 from qg_setup import define_orography, invent_state
 from qg_output import setup_output, output
 from datetime import datetime, timedelta
+from iris import load_cubes, Constraint
 
 # Model time step
 dt = float(const.dt0)
@@ -19,14 +20,19 @@ output_file = "neural_net.nc"
 simul_len = (end - start).total_seconds()
 date_range = [start + timedelta(seconds=i*dt) for i in range(int(simul_len/dt))]
 
-# Define model orography
-orog = define_orography()
+# Get initial condition
+time_constraint = Constraint(time=lambda t: t > start and t <= end)
+q_real, u_real, v_real = load_cubes("training_data.nc", ["pv", "u", "v"])
+q_real = q_real.extract(time_constraint)
+u_real = u_real.extract(time_constraint)
+v_real = v_real.extract(time_constraint)
+q_real.transpose()
+u_real.transpose()
+v_real.transpose()
 
-# Set up up initial stream function and boundary arrays
-x, x_north, x_south, q_north, q_south = invent_state(orog)
-
-# Get PV and wind from streamfunction
-q, u, v = prepare_integration(x, x_north, x_south, orog)
+q = q_real[...,0].data.copy()
+u = u_real[...,0].data.copy()
+v = v_real[...,0].data.copy()
 
 # Set up output NetCDF file and print zeroth time step
 setup_output(output_file, start)
